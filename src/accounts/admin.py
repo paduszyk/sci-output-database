72980ee46003431ba4805d9569d68e4e2243963a
Kamil Paduszyński <kpaduszy@gmail.com>
2022-06-17 01:12:08 +0200

    Override some common methods of the accounts.User model

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -13,3 +13,25 @@ admin.site.unregister(Group)
 @admin.register(User)
 class UserAdmin(AuthUserAdmin, admin.ModelAdmin):
     """Admin options for the User model."""
+
+    def get_fieldsets(self, request, obj):
+        fieldsets = list(super().get_fieldsets(request, obj))
+        if obj:
+            fieldsets = [
+                (None, {"fields": ["id"]}),
+                ("Informacje podstawowe", fieldsets[0][1]),
+                *fieldsets[1:],
+            ]
+        return fieldsets
+
+    def get_readonly_fields(self, request, obj):
+        return ["id", *list(super().get_readonly_fields(request, obj))]
+
+    def get_list_display(self, request):
+        return ["id", *list(super().get_list_display(request))]
+
+    def get_list_filter(self, request):
+        return ()
+
+    def get_ordering(self, request):
+        return ["last_name", "first_name", "id"]
