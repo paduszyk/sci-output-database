4309b2a677fe145d9156a4dc3c358337aee23569
Kamil Paduszyński <kpaduszy@gmail.com>
2022-06-17 12:55:11 +0200

    Distinguish elements package within the attainments app

--- src/attainments/urls.py
+++ src/attainments/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import include, path
+
+urlpatterns = [
+    path("articles/", include("attainments.elements.articles.urls")),
+    path("patents/", include("attainments.elements.patents.urls")),
+    path("grants/", include("attainments.elements.grants.urls")),
+]
